5366a9100d048b44477878f0495a5a3655031908
adore_blvnk <83258252+adoreblvnk@users.noreply.github.com>
2022-08-30 15:01:35 +0800

    feat: creatio ex nihilo

---
+++ README.md
@@ -0,0 +1,10 @@
+Requirements:
+
+- Any Python version between 3.6 & 3.9 inclusive.
+
+
+To run:
+
+    python -m venv venv
+    pip install -r requirements.txt
+    python app.py
---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask, flash, render_template
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/hosting")
+def hosting():
+    return render_template("hosting.html")
+
+@app.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+
+@app.route("/blog-details")
+def blog_details():
+    return render_template("blog-details.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
