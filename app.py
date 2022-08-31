5ff87a3338cf3aa5d7dec90624f9e7543ee9b9f0
adore_blvnk <83258252+adoreblvnk@users.noreply.github.com>
2022-08-31 09:46:29 +0800

    fix: uncomment run app code

--- app.py
+++ app.py
@@ -36,11 +36,11 @@ def blog():
 def contact():
     return render_template("contact.html")
 
-
+# add routes here
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
